bc5d20d13d45ac8873d05060eaaa9ecda3678601
Sulfikar Ali Nazar <sulfikar.ali.nazar@gmail.com>
2019-08-19 17:55:23 +0530

    Initialize

---
+++ src/main/java/org/bismi/core/App.java
@@ -0,0 +1,16 @@
+package org.bismi.core;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+       
+        
+        
+    }
+}
