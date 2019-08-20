7b3e3f2b95bd138f5d0f79c451ca2abf843ffa80
Sulfikar Ali Nazar <sulfikar.ali.nazar@gmail.com>
2019-08-20 15:31:25 +0530

    Removing

--- src/main/java/org/bismi/steps/Login.java
+++ src/main/java/org/bismi/steps/Login.java
@@ -15,6 +15,7 @@ public class Login {
 		System.out.println(" -------------------------------> ");
 		System.out.println(" -------------------------------> ");
 		System.out.println(" -------------------------------> ");
+		
 	}
 
 	@When("I see login page")
